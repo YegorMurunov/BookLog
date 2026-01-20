4caed0847397688d7bc85bb0eb9b43925c64204b
YegorMurunov <yegormurunov87@gmail.com>
2026-01-20 21:51:07 +0200

    optimized card component

--- src/components/ui/Card/Card.tsx
+++ src/components/ui/Card/Card.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import { memo } from 'react';
 
 import type { ICardProps } from '@/types/ui/card.interface';
 
@@ -7,35 +8,39 @@ import CardInfo from './CardInfo/CardInfo';
 
 import styles from './card.module.scss';
 
-const Card = ({
-	title,
-	mainValue,
-	subtext,
-	className,
-	subtextValue,
-	tooltipText,
-	index
-}: ICardProps) => {
-	return (
-		<div className={clsx(styles.card, className)}>
-			<div className={styles.title}>
-				{title}
-				{tooltipText && index && <CardInfo title={tooltipText} index={index} />}
+const Card = memo(
+	({
+		title,
+		mainValue,
+		subtext,
+		className,
+		subtextValue,
+		tooltipText,
+		index
+	}: ICardProps) => {
+		return (
+			<div className={clsx(styles.card, className)}>
+				<div className={styles.title}>
+					{title}
+					{tooltipText && index && (
+						<CardInfo title={tooltipText} index={index} />
+					)}
+				</div>
+				{
+					<AnimatedCounter
+						className={styles.value}
+						from={0}
+						to={mainValue}
+						duration={3}
+					/>
+				}
+				<div className={styles.subtext}>
+					{subtextValue && <span>{subtextValue}</span>}
+					{subtext}
+				</div>
 			</div>
-			{
-				<AnimatedCounter
-					className={styles.value}
-					from={0}
-					to={mainValue}
-					duration={3}
-				/>
-			}
-			<div className={styles.subtext}>
-				{subtextValue && <span>{subtextValue}</span>}
-				{subtext}
-			</div>
-		</div>
-	);
-};
+		);
+	}
+);
 
 export default Card;
